9378381a70d62b91ccf6976845def0010d6fb7c4
2968344096@qq.com <chensiyu@wisdombole.com>
2026-06-26 22:24:08 +0800

    Lara

---
+++ js/talk.js
@@ -0,0 +1,20 @@
+new TypeIt("#SiyuandLarara", {
+        loop: true,
+        cursorSpeed: 1000,
+        speed: 150
+    })
+    .type("Yuyu and Lara")
+    .pause(2000)
+    .delete(null, {
+        delay: 500
+    })
+    .type("The first gift I prepared for you...")
+    .pause(3000)
+    .go();
+
+new TypeIt('#talkToLara', {
+    lifeLike: true,
+    cursorSpeed: 1000,
+    waitUntilVisible: true,
+    speed: 1
+}).go();
